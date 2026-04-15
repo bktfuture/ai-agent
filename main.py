35a30e28887f8b524fcc862908bb0155e703f7ce
Aiturgan <aiturgan888@gmail.com>
2026-04-14 20:45:13 -0700

    added verbopse functionality for better developer and user  expirience

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import sys
-
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
-
+ 
     client = genai.Client(api_key=api_key)
 
     if len(sys.argv) <2:
         print("I need a prompt!")
         sys.exit(1)
-    prompt = sys.argv[1]
-    print("Args", sys.argv)
+    verbose_flag = False
 
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
+       
+
+    prompt = sys.argv[1]
+    # print("Args", sys.argv)
+
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
     response = client.models.generate_content(
         model='gemini-2.5-flash', contents=prompt
     )
@@ -25,7 +32,11 @@ def main():
     if response is None and response.usage_metadata is None:
         print("No response from the model")
         return 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 main()
